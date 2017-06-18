20dee44ae0a4c08e5f2c39099c23d3cb1557b7f8
Samuele Resca <samuele.resca@gmail.com>
2017-06-18 22:46:11 +0200

    Clean code

--- Modules/GameVars.ts
+++ Modules/GameVars.ts
@@ -2,7 +2,7 @@ export module GameVars {
  
  /* CONSTANTS */
 export const INTERVAL_GAME = 16
-export const INTERVAL_WORD = 2000
+export const INTERVAL_WORD = 3000
 export const DEFAULT_SPEED = 1
 export const LIVES = 5
 
